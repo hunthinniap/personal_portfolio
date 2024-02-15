5834e8c9a4c4304426b62b6668a6a850ad01ae47
HookahArtSpace <hunthinniap@gmail.com>
2024-02-15 02:58:27 -0600

    add update hook

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, url_for
+import git 
 
 app = Flask(__name__)
+
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./orbe')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
 
 @app.route('/')
 def home():
