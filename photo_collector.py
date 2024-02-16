e4d186bc5c5e78a70e1012570bc15f3a8d3d987f
HookahArtSpace <hunthinniap@gmail.com>
2024-02-16 01:24:38 -0600

    use api

--- photo_collector.py
+++ photo_collector.py
@@ -1,24 +1,23 @@
 # Replace 'owner', 'repo', and 'path_to_folder' with actual values
-from github import Github
-
-g = Github()
+import requests
 import json
 
 
 def get_cover_photos():
     owner = "hunthinniap"
-    repo_name = "portfolio_asset"
-    path_to_folder = "Photography/cover_photos"
-    repo = g.get_repo(f"{owner}/{repo_name}")
+    repo = "portfolio_asset"
+    path = "Photography/cover_photos"
 
+    url = f"https://api.github.com/repos/{owner}/{repo}/contents/{path}"
+    response = requests.get(url)
     # Get contents of the specified folder
-    contents = repo.get_contents(path_to_folder)
+    contents = response.json()
     photo_collections = []
     cover_collections = []
     for content_file in contents:
         photo_info = {}
-        photo_info["cover_url"] = content_file.download_url
-        photo_info["title"] = content_file.name[:-4]
+        photo_info["cover_url"] = content_file["download_url"]
+        photo_info["title"] = content_file["name"][:-4]
         photo_info["link"] = f'/photography/{photo_info["title"]}'
         photo_collections.append(photo_info)
         cover_collections.append(photo_info["title"])
@@ -30,17 +29,19 @@ def get_cover_photos():
 
 def get_collection_details(collection_name):
     owner = "hunthinniap"
-    repo_name = "portfolio_asset"
-    path_to_folder = f"Photography/{collection_name}"
+    repo = "portfolio_asset"
+    path = f"Photography/{collection_name}"
 
-    repo = g.get_repo(f"{owner}/{repo_name}")
-
+    url = f"https://api.github.com/repos/{owner}/{repo}/contents/{path}"
+    response = requests.get(url)
     # Get contents of the specified folder
-    contents = repo.get_contents(path_to_folder)
+    contents = response.json()
+    # Get contents of the specified folder
+    contents = response.json()
     photo_collections = []
     for content_file in contents:
         photo_info = {}
-        photo_info["src"] = content_file.download_url
+        photo_info["src"] = content_file["download_url"]
         photo_info["alt"] = ""
         photo_collections.append(photo_info)
 
@@ -49,6 +50,8 @@ def get_collection_details(collection_name):
 
 
 if __name__ == "__main__":
+
     cover_collections = get_cover_photos()
+
     for collection_name in cover_collections:
         get_collection_details(collection_name)
