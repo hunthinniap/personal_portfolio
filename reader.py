7ad91b2173e167b864c061c565452e1086318df4
HookahArtSpace <hunthinniap@gmail.com>
2024-02-18 03:05:09 -0600

    add music page

--- reader.py
+++ reader.py
@@ -3,20 +3,36 @@ import requests
 
 from text_data import page_alt, page_description
 
+base_url = "https://raw.githubusercontent.com/hunthinniap/portfolio_asset/main/"
+
 
 def read_cover():
-    url = "https://raw.githubusercontent.com/hunthinniap/portfolio_asset/main/Photography/cover_photos/photos.json"
+    url = f"{base_url}Photography/cover_photos/photos.json"
     response = requests.get(url)
     cover_photos = json.loads(response.text)
     return cover_photos
 
 
 def read_collections(collection_name):
-    url = f"https://raw.githubusercontent.com/hunthinniap/portfolio_asset/main/Photography/{collection_name}/photos.json"
+    url = f"{base_url}Photography/{collection_name}/photos.json"
 
     response = requests.get(url)
     collections = json.loads(response.text)
     return collections
+
+
+def read_music_cover():
+    url = f"{base_url}music_cover/photos.json"
+    response = requests.get(url)
+    cover_photos = json.loads(response.text)
+    return cover_photos
+
+
+def read_music_specs(music_name):
+    url = f"{base_url}/music/{music_name}/specs.json"
+    response = requests.get(url)
+    specs = json.loads(response.text)
+    return specs
 
 
 def read_page_title(page):
@@ -25,6 +41,3 @@ def read_page_title(page):
 
 def read_page_description(page):
     return page_description.get(page, "")
-
-
-
